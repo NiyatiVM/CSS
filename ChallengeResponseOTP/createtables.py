bf65b1387004a3ef6e626bb16f91fb1ae6baeec4
NiyatiVM <n.mervana@somaiya.edu>
2020-09-25 17:12:02 +0530

    Changes

--- ChallengeResponseOTP/createtables.py
+++ ChallengeResponseOTP/createtables.py
@@ -10,19 +10,12 @@ con = lite.connect('client.db')
 
 with con:
     cur = con.cursor()    
-    #cur.execute("CREATE TABLE Clients(identity INTEGER PRIMARY KEY, functionnum INTEGER UNIQUE)")
-    cur.execute("DELETE FROM Clients")
-    con.commit()
-    # cur.execute("SELECT * FROM Clients")
-    # ans=cur.fetchall()
-    # for i in ans:
-    # 	print(i)
+    cur.execute("CREATE TABLE Clients(identity INTEGER PRIMARY KEY, functionnum INTEGER UNIQUE)")
+
 con = lite.connect('server.db')
 
 # with con:
-#     cur = con.cursor()
-#     cur.execute("DROP TABLE Servers")
 with con:
     cur = con.cursor() 
-    cur.execute("DELETE FROM Servers")
-    #cur.execute("CREATE TABLE Servers(identity INTEGER PRIMARY KEY, functionnum INTEGER UNIQUE)")
+    #cur.execute("DELETE FROM Servers")
+    cur.execute("CREATE TABLE Servers(identity INTEGER PRIMARY KEY, functionnum INTEGER UNIQUE)")
